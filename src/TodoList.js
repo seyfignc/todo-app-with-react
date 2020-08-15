b53203d5eb9860741ea60d3a48f6e505368f7332
Seyfi GENÇ <seyfi.genc21@gmail.com>
2020-08-15 15:39:45 +0300

    TodoLine style created

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,12 +3,19 @@ import React, { Component } from 'react';
 class TodoList extends Component {
     render() {
         const listItems = this.props.items;
+        const TodoLine={
+            backgroundColor:"red",
+            color:"yellow",
+            width:"300px",
+            margin:"0 auto",
+            display:"flex"
+        }
         return (
             <div>
                 {
                     listItems.map(item => {
                         return (
-                            <div key={item.id} style={{backgroundColor:"red",color:"yellow",width:"200px",margin:"0 auto", display:"flex"}}>
+                            <div key={item.id} style={TodoLine}>
                                 <div style={item.IsCompleted?{textDecoration:"line-through"}:{textDecoration:"none"}}>{item.value}</div>
                                 <div><button onClick={()=>this.props.itemDeleted(item)}>Sil</button></div>
                                 <div><button onClick={()=>this.props.itemCompleted(item)}>Tamamla</button></div>
